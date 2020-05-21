b0b25df5561e433d4a2d4230e690bd7a93e774e4
Ashwin R C <ashwin2001achu@gmail.com>
2020-05-21 08:02:00 +0000

    guacamole: POSP bringup

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -11,12 +11,6 @@ DEVICE_PATH := device/oneplus/guacamole
 # Assert
 TARGET_OTA_ASSERT_DEVICE := OnePlus7Pro
 
-# Display
-TARGET_SCREEN_DENSITY := 560
-
-# Kernel
-TARGET_KERNEL_CONFIG := vendor/sm8150-perf_defconfig
-
 # Partitions
 BOARD_BOOTIMAGE_PARTITION_SIZE := 100663296
 BOARD_CACHEIMAGE_PARTITION_SIZE := 268435456
